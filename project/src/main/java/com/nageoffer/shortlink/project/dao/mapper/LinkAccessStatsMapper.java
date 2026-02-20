ed59c1032a400c108b26f87e7584c1b774e18766
雪银星月 <425899311@qq.com>
2026-02-20 14:11:12 +0800

    feature: 开发短链接监控之地区访问数据

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -3,6 +3,7 @@ package com.nageoffer.shortlink.project.dao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nageoffer.shortlink.project.dao.entity.LinkAccessStatsDO;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 短链接基础访问监控持久层
