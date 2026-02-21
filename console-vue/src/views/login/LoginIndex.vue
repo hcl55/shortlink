0cabdfbce5d82c4b072de602e68ef1d6d3526c92
雪银星月 <425899311@qq.com>
2026-02-21 17:50:17 +0800

    修改前端登录页

--- console-vue/src/views/login/LoginIndex.vue
+++ console-vue/src/views/login/LoginIndex.vue
@@ -2,17 +2,15 @@
   <div class="login-page">
     <h1 class="title">短 链 分 享 系 统</h1>
     <div class="login-box">
-      <!-- 登录 -->
       <div class="logon" :class="{ hidden: !isLogin }">
         <h2>用户登录</h2>
         <el-form ref="loginFormRef1" :model="loginForm" label-width="50px" :rules="loginFormRule">
           <div class="form-container1">
-            <el-form-item prop="phone">
+            <el-form-item prop="username">
               <el-input v-model="loginForm.username" placeholder="请输入用户名" maxlength="11" show-word-limit clearable>
                 <template v-slot:prepend> 用户名 </template>
               </el-input>
             </el-form-item>
-
             <el-form-item prop="password">
               <el-input v-model="loginForm.password" type="password" clearable placeholder="请输入密码" show-password
                 style="margin-top: 20px">
@@ -23,16 +21,15 @@
           <div class="btn-gourp">
             <div>
               <el-checkbox class="remeber-password" v-model="checked"
-                style="color: #a0a0a0; margin: 0 0 0px 0">记住密码</el-checkbox>
+                style="color: #64748b; margin: 0">记住密码</el-checkbox>
             </div>
             <div>
-              <el-button :loading="loading" @keyup.enter="login" type="primary" plain
+              <el-button :loading="loading" @keyup.enter="login" type="primary"
                 @click="login(loginFormRef1)">登录</el-button>
             </div>
           </div>
         </el-form>
       </div>
-      <!-- 注册 -->
       <div class="register" :class="{ hidden: isLogin }">
         <h2>用户注册</h2>
         <el-form ref="loginFormRef2" :model="addForm" label-width="50px" class="form-container" width="width"
@@ -57,50 +54,32 @@
               <template v-slot:prepend> 姓<span class="second-font">名</span> </template>
             </el-input>
           </el-form-item>
-
           <el-form-item prop="password">
             <el-input v-model="addForm.password" type="password" clearable placeholder="请输入密码" show-password>
               <template v-slot:prepend> 密<span class="second-font">码</span> </template>
             </el-input>
           </el-form-item>
-          <!-- 验证码 -->
-          <!-- <el-form-item prop="vertify_code">
-            <el-input
-              v-model="loginForm.vertify_code"
-              placeholder="验证码"
-              prefix-icon="el-icon-key"
-              clearable
-            >
-              <template v-slot:append>
-                <div class="login-code" @click="refreshCode" title="看不清？点击切换">
-                  <vertify-code :identifyCode="loginIdentifyCode"></vertify-code>
-                </div>
-              </template>
-            </el-input>
-          </el-form-item> -->
           <div class="btn-gourp">
             <div></div>
             <div>
-              <el-button :loading="loading" @keyup.enter="login" type="primary" plain
+              <el-button :loading="loading" @keyup.enter="login" type="primary"
                 @click="addUser(loginFormRef2)">注册</el-button>
             </div>
           </div>
         </el-form>
       </div>
-      <!-- 左右移动的切换按钮 -->
       <div class="move" ref="moveRef">
-        <span style="font-size: 18px; margin-bottom: 25px; color: rgb(225, 238, 250)">{{
+        <span style="font-size: 18px; margin-bottom: 25px; color: #f8fafc; font-weight: 300;">{{
           !isLogin ? '已有账号？' : '还没有账号？'
         }}</span>
-        <span style="font-size: 16px; color: rgb(225, 238, 250)">{{
-          !isLogin ? '欢迎登录账号！' : '欢迎注册账号！'
+        <span style="font-size: 14px; color: rgba(248, 250, 252, 0.7); letter-spacing: 1px;">{{
+          !isLogin ? '欢迎登录账号' : '欢迎注册账号'
         }}</span>
-        <el-button style="width: 100px; margin-top: 30px" @click="changeLogin">{{
+        <el-button class="switch-btn" @click="changeLogin">{{
           !isLogin ? '去登录' : '去注册'
         }}</el-button>
       </div>
     </div>
-    <div ref="vantaRef" class="vanta"></div>
   </div>
   <el-dialog v-model="isWC" title="人机验证" width="40%" :before-close="handleClose">
     <div class="verification-flex">
@@ -119,84 +98,60 @@
       </span>
     </template>
   </el-dialog>
-  <!-- </template> -->
 </template>
 
 <script setup>
 import { setToken, setUsername, getUsername } from '@/core/auth.js'
-import { ref, reactive, onMounted, onBeforeUnmount, watch, getCurrentInstance } from 'vue'
+import { ref, reactive, getCurrentInstance } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
-import * as THREE from 'three'
-import WAVES from 'vanta/src/vanta.waves'
+
 const { proxy } = getCurrentInstance()
 const API = proxy.$API
 const loginFormRef1 = ref()
 const loginFormRef2 = ref()
 const router = useRouter()
-const loginForm = reactive({
-  username: '',
-  password: '',
-})
-const addForm = reactive({
-  username: '',
-  password: '',
-  realName: '',
-  phone: '',
-  mail: ''
-})
+const loginForm = reactive({ username: '', password: '' })
+const addForm = reactive({ username: '', password: '', realName: '', phone: '', mail: '' })
 
 const addFormRule = reactive({
   phone: [
     { required: true, message: '请输入手机号', trigger: 'blur' },
-    {
-      pattern: /^1[3|5|7|8|9]\d{9}$/,
-      message: '请输入正确的手机号',
-      trigger: 'blur'
-    },
+    { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '请输入正确的手机号', trigger: 'blur' },
     { min: 11, max: 11, message: '手机号必须是11位', trigger: 'blur' }
   ],
-  username: [{ required: true, message: '请输入您的真实姓名', trigger: 'blur' }],
+  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
   password: [
     { required: true, message: '请输入密码', trigger: 'blur' },
     { min: 8, max: 15, message: '密码长度请在八位以上', trigger: 'blur' }
   ],
   mail: [
     { required: true, message: '请输入邮箱', trigger: 'blur' },
-    {
-      pattern: /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
-      message: '请输入正确的邮箱号',
-      trigger: 'blur'
-    }
+    { pattern: /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/, message: '请输入正确的邮箱号', trigger: 'blur' }
   ],
-  realName: [
-    { required: true, message: '请输姓名', trigger: 'blur' },
-  ]
+  realName: [{ required: true, message: '请输入姓名', trigger: 'blur' }]
 })
+
 const loginFormRule = reactive({
-  username: [{ required: true, message: '请输入您的真实姓名', trigger: 'blur' }],
+  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
   password: [
     { required: true, message: '请输入密码', trigger: 'blur' },
     { min: 8, max: 15, message: '密码长度请在八位以上', trigger: 'blur' }
   ],
 })
-// 注册
+
 const addUser = (formEl) => {
   if (!formEl) return
   formEl.validate(async (valid) => {
     if (valid) {
-      // 检测用户名是否已经存在
       const res1 = await API.user.hasUsername({ username: addForm.username })
       if (res1.data.success !== false) {
-        // 注册
         const res2 = await API.user.addUser(addForm)
-        // console.log(res2)
         if (res2.data.success === false) {
           ElMessage.warning(res2.data.message)
         } else {
           const res3 = await API.user.login({ username: addForm.username, password: addForm.password })
           const token = res3?.data?.data?.token
-          // 将username和token保存到cookies中和localStorage中
           if (token) {
             setToken(token)
             setUsername(addForm.username)
@@ -209,21 +164,14 @@ const addUser = (formEl) => {
       } else {
         ElMessage.warning('用户名已存在！')
       }
-    } else {
-      return false
     }
   })
-
 }
-// 公众号验证码
+
 const isWC = ref(false)
 const verificationRef = ref()
-const verification = reactive({
-  code: ''
-})
-const verificationRule = reactive({
-  code: [{ required: true, message: '请输入验证码', trigger: 'blur' }]
-})
+const verification = reactive({ code: '' })
+const verificationRule = reactive({ code: [{ required: true, message: '请输入验证码', trigger: 'blur' }] })
 const verificationLogin = (formEl) => {
   if (!formEl) return
   formEl.validate(async (valid) => {
@@ -233,7 +181,6 @@ const verificationLogin = (formEl) => {
       const res1 = await API.user.login(loginForm)
       if (res1.data.code === '0') {
         const token = res1?.data?.data?.token
-        // 将username和token保存到cookies中和localStorage中
         if (token) {
           setToken(token)
           setUsername(loginForm.username)
@@ -243,7 +190,6 @@ const verificationLogin = (formEl) => {
         ElMessage.success('登录成功！')
         router.push('/home')
       } else if (res1.data.message === '用户已登录') {
-        // 如果已经登录了，判断一下浏览器保存的登录信息是不是再次登录的信息，如果是就正常登录
         const cookiesUsername = getUsername()
         if (cookiesUsername === loginForm.username) {
           ElMessage.success('登录成功！')
@@ -258,7 +204,7 @@ const verificationLogin = (formEl) => {
     }
   })
 }
-// 登录
+
 const login = (formEl) => {
   if (!formEl) return
   formEl.validate(async (valid) => {
@@ -266,7 +212,6 @@ const login = (formEl) => {
       const res1 = await API.user.login(loginForm)
       if (res1.data.code === '0') {
         const token = res1?.data?.data?.token
-        // 将username和token保存到cookies中和localStorage中
         if (token) {
           setToken(token)
           setUsername(loginForm.username)
@@ -276,7 +221,6 @@ const login = (formEl) => {
         ElMessage.success('登录成功！')
         router.push('/home')
       } else if (res1.data.message === '用户已登录') {
-        // 如果已经登录了，判断一下浏览器保存的登录信息是不是再次登录的信息，如果是就正常登录
         const cookiesUsername = getUsername()
         if (cookiesUsername === loginForm.username) {
           ElMessage.success('登录成功！')
@@ -287,41 +231,14 @@ const login = (formEl) => {
       } else if (res1.data.message === '用户不存在') {
         ElMessage.error('请输入正确的账号密码!')
       }
-    } else {
-      return false
     }
   })
-
-
 }
 
 const loading = ref(false)
-// 是否记住密码
 const checked = ref(true)
-const vantaRef = ref()
-// 动态背景
-let vantaEffect = null
-onMounted(() => {
-  vantaEffect = WAVES({
-    el: vantaRef.value,
-    THREE: THREE,
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.0,
-    minWidth: 200.0,
-    scale: 1.0,
-    scaleMobile: 1.0
-  })
-})
-onBeforeUnmount(() => {
-  if (vantaEffect) {
-    vantaEffect.destroy()
-  }
-})
-// 展示登录还是展示注册
 const isLogin = ref(true)
-const moveRef = ref() // 左右移动的切换按钮模块
+const moveRef = ref()
 const changeLogin = () => {
   isLogin.value = !isLogin.value
   if (isLogin.value) {
@@ -333,123 +250,67 @@ const changeLogin = () => {
 </script>
 
 <style lang="less" scoped>
-.login-box {
-  border: 2px solid #0984e3;
+.login-page {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
   overflow: hidden;
+  background: radial-gradient(circle at center, #1e293b 0%, #0f172a 100%);
+}
+
+.login-box {
+  background: #ffffff;
   display: flex;
   justify-content: space-between;
-  border-radius: 20px;
-  padding: 0 40px 0 40px;
+  border-radius: 8px;
+  padding: 0 40px;
   width: 700px;
-  // background-color: #eee;
+  height: 480px;
   position: absolute;
   z-index: 999;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   box-sizing: border-box;
-  // border: 1px solid #eee;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  background-color: #fff;
-  animation: hideIndex 0.5s;
+  box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4);
+  animation: fadeIn 0.5s ease-out;
 
   h2 {
-    font-size: 30px;
-    font-family:
-      PingFangSC-Semibold,
-      PingFang SC;
+    font-size: 24px;
+    font-family: "PingFang SC", sans-serif;
     font-weight: 600;
-    color: #3a3f63;
+    color: #1e293b;
     width: 100%;
-    text-align: center;
-    padding: 20px;
+    text-align: left;
+    padding: 45px 0 25px 0;
   }
 
   .el-form-item {
-    margin-bottom: 23px;
+    margin-bottom: 22px;
+  }
+
+  :deep(.el-input-group__prepend) {
+    background-color: #f1f5f9;
+    color: #475569;
+    border-right: none;
   }
 
   .btn-gourp {
-    margin-top: 30px;
+    margin-top: 25px;
     display: flex;
     justify-content: space-between;
-    margin-bottom: 20px;
+    align-items: center;
 
     .el-button {
-      width: 100px;
+      width: 120px;
+      height: 42px;
+      background: #0f172a;
+      border: none;
+      border-radius: 4px;
+      &:hover {
+        background: #334155;
+      }
     }
-
-    .remeber-password {
-      left: 0;
-      line-height: 0.5rem;
-    }
-  }
-
-  .el-checkbox {
-    width: 100%;
-    text-align: center;
-    margin-top: 1rem;
-  }
-}
-
-/deep/ .el-form-item__content {
-  margin-left: 0 !important;
-}
-
-@keyframes hideIndex {
-
-  // <!--具体细节自己可以调整-->
-  0% {
-    opacity: 0;
-    transform: translate(7.3125rem, -50%);
-  }
-
-  100% {
-    opacity: 1;
-    transform: translate(-50%, -50%);
-  }
-}
-
-.login-page {
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-}
-
-.vanta {
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  z-index: 0;
-}
-
-.logon {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.hidden {
-  animation: hidden 1s;
-  animation-fill-mode: forwards; // 保持最后的状态
-}
-
-@keyframes hidden {
-
-  // <!--具体细节自己可以调整-->
-  0% {
-    opacity: 1;
-  }
-
-  70% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 0;
   }
 }
 
@@ -461,16 +322,22 @@ const changeLogin = () => {
   flex-direction: column;
   justify-content: center;
   width: 40%;
-  transition-duration: 0.5s;
+  transition: transform 0.5s cubic-bezier(0.4, 0, 0.2, 1);
   align-items: center;
-  background: #06beb6;
-  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #0984e3, #0984e3);
-  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right,
-      #1a8fd5,
-      #0984e3);
-  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: #1e293b;
+
+  .switch-btn {
+    background: transparent;
+    color: #fff;
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    width: 100px;
+    margin-top: 30px;
+    border-radius: 4px;
+    &:hover {
+      background: rgba(255, 255, 255, 0.1);
+      border-color: #fff;
+    }
+  }
 }
 
 .title {
@@ -479,35 +346,29 @@ const changeLogin = () => {
   transform: translateX(-50%);
   top: 15%;
   z-index: 999;
-  font-size: 40px;
-  color: #fff;
-  font-weight: bolder;
+  font-size: 32px;
+  color: #f8fafc;
+  font-weight: 200;
+  letter-spacing: 10px;
 }
 
-:deep(.el-input__suffix-inner) {
-  width: 60px;
+.logon, .register {
+  width: 55%;
+  display: flex;
+  flex-direction: column;
 }
 
-.form-container1 {
-  transform: translateY(-80%);
+.hidden {
+  opacity: 0;
+  pointer-events: none;
+}
+
+@keyframes fadeIn {
+  from { opacity: 0; transform: translate(-50%, -48%); }
+  to { opacity: 1; transform: translate(-50%, -50%); }
 }
 
 .second-font {
   margin-left: 13px;
-}
-
-.verification-flex {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  .img {
-    margin-top: 10px;
-    align-self: center;
-  }
-  .form {
-    transform: translateY(15px);
-    width: 90%;
-  }
 }
 </style>
